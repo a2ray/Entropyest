7547d6284e3c4d85eb5c481396b534c2089efbad
Anandaroop Ray <a2ray@ucsd.edu>
2022-12-26 17:08:46 +1100

    fails randomly in kliep dre fit

--- src/Entropyest.jl
+++ src/Entropyest.jl
@@ -1,44 +1,34 @@
 module Entropyest
 using DensityRatioEstimation, Statistics, HiQGA.transD_GP, Distributed, DelimitedFiles, Dates, Random
 
-function getkldfromsamples(x1, x2; σ=[0.5], b=[20], nfolds=10, debug=false, Kfolds=5)
+function getkldfromsamples(x1, x2; σ=[0.5], b=[20], nfolds=10, debug=false, nfit=6000)
     t = time()
     nx1, nx2 = length(x1), length(x2)
-    debug && (@info "fitting $nx1, $nx2 samples")
-    nperfold_x1, nperfold_x2 = map(nx->getnperfold(nx, Kfolds), (nx1, nx2))
-    debug && (@info "perfold are $nperfold_x1, $nperfold_x2 samples")
-    ridx_x1, ridx_x2 = map(nx->getrandomizedidx(nx), (nx1, nx2))
-    K_dre = Vector{Any}(undef, Kfolds)
-    KLD = zeros(Kfolds)
-    for k = 1:Kfolds
-        idx_x1 = ridx_x1[getfoldidx(k, nperfold_x1, Kfolds, nx1)]
-        idx_x2 = ridx_x2[getfoldidx(k, nperfold_x2, Kfolds, nx2)]
-        K_dre[k] = fit(KLIEP, x1[idx_x1], x2[idx_x2], LCV((;σ,b), nfolds))
-        rfunc = densratiofunc(x1[idx_x1], x2[idx_x2], K_dre[k])
-        KLD[k] = mean(log.(rfunc.(x1[idx_x1])))
-    end    
+    nfit = min(nx1, nx2, nfit)
+    debug && (@info "fitting $nfit samples")
+    K_dre = fit(KLIEP, x1[randperm(nx1)[1:nfit]], x2[randperm(nx2)[1:nfit]], LCV((;σ,b), nfolds))
+    # lr = ln(p(x1)/q(x2))
+    rfunc = densratiofunc(x1, x2, K_dre) 
+    # KLD(x1~p1||x2~q) = expectation of lr when samples are drawn from x1
+    KLD = mean(log.(rfunc.(x1)))
     debug && (@info "process $(myid()) took $(time()-t) seconds")
-    [mean(KLD), mean(getproperty.(K_dre, :σ)), mean(getproperty.(K_dre, :b))]
+    [KLD, K_dre.σ, K_dre.b]
 end    
 
-getrandomizedidx(totalnum) = randperm(totalnum)
-getnperfold(totalnum, K) = floor(Int, totalnum/K)
-getfoldidx(k, nperfold, K, totalnum) = k < K ? ((k-1)nperfold + 1 : k*nperfold) : ((K-1)nperfold + 1 : totalnum) 
-
 function getkldfromopt(opt::transD_GP.Options, x2::AbstractVector, pids::UnitRange; 
-            σ=[0.5], b=[20], nfolds=2, burninfrac=0.5, debug=false, restrictto=2, nuse=6000, Kfolds=5)
+            σ=[0.5], b=[20], nfolds=2, burninfrac=0.5, debug=false, restrictto=2, nuse=6000, nfit=6000)
     # open file
-    debug && @info "OPENING "*opt.fdataname*"at pids $pids with burin: $burninfrac at $(Dates.now())"
+    debug && @info "OPENING "*opt.fdataname*"at pids $pids at $(Dates.now())"
     x1 = reduce(vcat, transD_GP.CommonToAll.assembleTat1(opt, :fstar; burninfrac, temperaturenum=1))
     x2 = reduce(vcat, x2)
     @assert size(x1, 2) == size(x2, 2) # same number of variables
-    nx1, nx2 = size(x1,1), size(x2,1) 
+    nx1, nx2 = size(x1,1), size(x2,1) # ensure there aren't too many samples
     debug && ((x1, x2) = map(x->x[:,1:restrictto], (x1, x2)))
-    nx1, nx2 = map(n -> ( n > nuse ? nuse : n ), (nx1, nx2))  # make sure nsamples isn't too huge
-    debug && (@info "using $nx1, $nx2 samples")
-    x1, x2 = map((x, n) -> x[1:n,:], (x1, x2), (nx1, nx2))
+    nuse = min(nx1, nx2, nuse)
+    debug && (@info "using $nuse samples")
+    x1, x2 = map(x->x[1:nuse,:], (x1, x2))
     # get kld from prior samples in x2
-    A = reduce(hcat, pmap((x, y)->getkldfromsamples(x, y; σ, b, nfolds, Kfolds, debug), 
+    A = reduce(hcat, pmap((x, y)->getkldfromsamples(x, y; σ, b, nfolds, nfit, debug), 
                                     WorkerPool(collect(pids)), eachcol(x1), eachcol(x2)))'
     debug && @info "WRITING "*opt.fdataname*" at $(Dates.now())"
     writedlm(opt.fdataname*"kld.txt", A)
@@ -47,7 +37,7 @@ end
 
 function getkldfromfilenames(fnames::Vector{String}, opt_in::transD_GP.Options, x2::AbstractVector; 
                         burninfrac=0.5, σ=[0.5], b=[20], nfolds=10, 
-                        debug=false, ncorespersounding=3, nuse=6000, Kfolds=5)
+                        debug=false, ncorespersounding=3, nuse=6000, nfit=6000)
     nsoundings = length(fnames)
     ncores = nworkers()
     nsequentialiters, nparallelsoundings = splittasks(;nsoundings, ncores, ncorespersounding)
@@ -60,7 +50,7 @@ function getkldfromfilenames(fnames::Vector{String}, opt_in::transD_GP.Options, 
             opt = deepcopy(opt_in)
             opt.fdataname = fnames[s]*"_"
             @async remotecall_wait(getkldfromopt, pids[1], opt, x2, pids[2:end]; 
-                                    σ, b, nfolds, burninfrac, nuse, Kfolds, debug)
+                                    σ, b, nfolds, burninfrac, nuse, nfit, debug)
         end # @sync
         @info "done $iter out of $nsequentialiters at $(Dates.now())"
     end
@@ -74,7 +64,6 @@ end
 
 ## parallel stuff
 
-# global splitters across soundings
 function splittasks(;nsoundings=nothing, ncores=nothing, ncorespersounding=nothing)
     # split into sequential iterations of parallel soundings
     @assert !any(isnothing.([nsoundings, ncores, ncorespersounding]))
