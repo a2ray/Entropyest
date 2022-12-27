f6f691861dbd28017b17112086410d8bd266014c
Anandaroop Ray <a2ray@ucsd.edu>
2022-12-27 19:28:05 +1100

    working with kliep timeouts

--- src/Entropyest.jl
+++ src/Entropyest.jl
@@ -1,12 +1,10 @@
 module Entropyest
 using DensityRatioEstimation, Statistics, HiQGA.transD_GP, Distributed, DelimitedFiles, Dates, Random
 
-function getkldfromsamples(x1, x2; σ=[0.5], b=[20], nfolds=10, debug=false, nfit=6000)
+function getkldfromsamples(x1, x2; σ=[0.5], b=[20], nfolds=10, debug=false)
     t = time()
     nx1, nx2 = length(x1), length(x2)
-    nfit = min(nx1, nx2, nfit)
-    debug && (@info "fitting $nfit samples")
-    K_dre = fit(KLIEP, x1[randperm(nx1)[1:nfit]], x2[randperm(nx2)[1:nfit]], LCV((;σ,b), nfolds))
+    K_dre = fit(KLIEP, x1, x2, LCV((;σ,b), nfolds))
     # lr = ln(p(x1)/q(x2))
     rfunc = densratiofunc(x1, x2, K_dre) 
     # KLD(x1~p1||x2~q) = expectation of lr when samples are drawn from x1
@@ -16,19 +14,19 @@ function getkldfromsamples(x1, x2; σ=[0.5], b=[20], nfolds=10, debug=false, nfi
 end    
 
 function getkldfromopt(opt::transD_GP.Options, x2::AbstractVector, pids::UnitRange; 
-            σ=[0.5], b=[20], nfolds=2, burninfrac=0.5, debug=false, restrictto=2, nuse=6000, nfit=6000)
+            σ=[0.5], b=[20], nfolds=2, burninfrac=0.5, debug=false, restrictto=2, nuse=6000)
     # open file
     debug && @info "OPENING "*opt.fdataname*"at pids $pids at $(Dates.now())"
     x1 = reduce(vcat, transD_GP.CommonToAll.assembleTat1(opt, :fstar; burninfrac, temperaturenum=1))
     x2 = reduce(vcat, x2)
     @assert size(x1, 2) == size(x2, 2) # same number of variables
-    nx1, nx2 = size(x1,1), size(x2,1) # ensure there aren't too many samples
     debug && ((x1, x2) = map(x->x[:,1:restrictto], (x1, x2)))
-    nuse = min(nx1, nx2, nuse)
-    debug && (@info "using $nuse samples")
-    x1, x2 = map(x->x[1:nuse,:], (x1, x2))
+    nx1, nx2 = size(x1, 1), size(x2, 1)
+    nx1, nx2 = map(n -> ( n > nuse ? nuse : n ), (nx1, nx2))  # make sure nsamples isn't too huge
+    debug && (@info "using $nx1, $nx2 samples")
+    x1, x2 = map((x, n) -> x[randperm(size(x, 1))[1:n],:], (x1, x2), (nx1, nx2)) # shuffle to break correlations in McMC
     # get kld from prior samples in x2
-    A = reduce(hcat, pmap((x, y)->getkldfromsamples(x, y; σ, b, nfolds, nfit, debug), 
+    A = reduce(hcat, pmap((x, y)->getkldfromsamples(x, y; σ, b, nfolds, debug), 
                                     WorkerPool(collect(pids)), eachcol(x1), eachcol(x2)))'
     debug && @info "WRITING "*opt.fdataname*" at $(Dates.now())"
     writedlm(opt.fdataname*"kld.txt", A)
@@ -37,7 +35,7 @@ end
 
 function getkldfromfilenames(fnames::Vector{String}, opt_in::transD_GP.Options, x2::AbstractVector; 
                         burninfrac=0.5, σ=[0.5], b=[20], nfolds=10, 
-                        debug=false, ncorespersounding=3, nuse=6000, nfit=6000)
+                        debug=false, ncorespersounding=3, nuse=6000)
     nsoundings = length(fnames)
     ncores = nworkers()
     nsequentialiters, nparallelsoundings = splittasks(;nsoundings, ncores, ncorespersounding)
@@ -50,7 +48,7 @@ function getkldfromfilenames(fnames::Vector{String}, opt_in::transD_GP.Options, 
             opt = deepcopy(opt_in)
             opt.fdataname = fnames[s]*"_"
             @async remotecall_wait(getkldfromopt, pids[1], opt, x2, pids[2:end]; 
-                                    σ, b, nfolds, burninfrac, nuse, nfit, debug)
+                                    σ, b, nfolds, burninfrac, nuse, debug)
         end # @sync
         @info "done $iter out of $nsequentialiters at $(Dates.now())"
     end
